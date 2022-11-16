2a3597cd254d87334489ca2d943eb96b306dd111
Mahad Mubashir <94633830+mahadmubashir@users.noreply.github.com>
2022-11-16 23:05:25 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,15 +4,16 @@ import { BrowserRouter as Router, Routes, Route} from "react-router-dom";
 import Login from './components/Login';
 import Product from './components/Product';
 import UserHome from './components/UserHome';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
     <Router>
       <div className="App" style={{height:"100%", backgroundColor:"white", position:"relative", width:"100%", backgroundSize: "cover"}}>
         <Routes>
-          <Route path="/" element={ <UserHome/> }/>
+          <Route path="/" element={<> <Navbar/> <UserHome/> </>}/>
           <Route path='/login' element={ <Login/> }/>
-          <Route path='/product' element={ <Product/> }/>
+          <Route path='/product' element={<><Navbar/> <Product/> </>}/>
         </Routes>
       </div>
     </Router>
