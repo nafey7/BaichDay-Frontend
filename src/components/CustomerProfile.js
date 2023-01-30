639685310e8a2ddbd4ed807377c07f4e272726a2
Mahad Mubashir <94633830+mahadmubashir@users.noreply.github.com>
2023-01-30 22:08:31 +0500

    profile

--- src/components/CustomerProfile.js
+++ src/components/CustomerProfile.js
@@ -8,11 +8,18 @@ function CustomerProfile() {
     // let userID = reactLocalStorage.get('userID', "", true);
     // const navigate = useNavigate();
     // const [cust, setCust] = React.useState({
-    //     // address: '',
-    //     // contact: '',
-    //     userID: username,
-    //     // password: 'password',
-    //     // name: '',
+    //     firstName: '',
+    //     lastName: '',
+    //     emailAddress: '',
+    //     password: 'password',
+    //     phoneNumber: '',
+    //     address: '',
+    //     city: '',
+    //     state: '',
+    //     country: '',
+    //     zipcode: '',
+    //     userID: userID
+
     // })
 
     // function deleteaccount() {
@@ -25,24 +32,49 @@ function CustomerProfile() {
             
     // }
 
-    // function changepassword(e){
+    // function changeFirstName(e){
     //     let x = cust;
-    //     x.password = e;
+    //     x.firstName = e;
     //     setCust(x)
     // }
-    // function changeaddress(e){
+    // function changeLastName(e){
+    //     let x = cust;
+    //     x.lastName = e;
+    //     setCust(x)
+    // }
+    // function changeEmail(e){
+    //     let x = cust;
+    //     x.emailAddress = e;
+    //     setCust(x)
+    // }
+    // function changePassword(e){
     //     let x = cust;
     //     x.address = e;
     //     setCust(x)
     // }
-    // function changename(e){
+    // function changePhoneNumber(e){
     //     let x = cust;
-    //     x.name = e;
+    //     x.phoneNumber = e;
     //     setCust(x)
     // }
-    // function changecontact(e){
+    // function changeAddress(e){
     //     let x = cust;
-    //     x.contact = e;
+    //     x.address = e;
+    //     setCust(x)
+    // }
+    // function changeCity(e){
+    //     let x = cust;
+    //     x.city = e;
+    //     setCust(x)
+    // }
+    // function changeState(e){
+    //     let x = cust;
+    //     x.state = e;
+    //     setCust(x)
+    // }
+    // function changeCountry(e){
+    //     let x = cust;
+    //     x.country = e;
     //     setCust(x)
     // }
     // function done(){
@@ -69,30 +101,44 @@ function CustomerProfile() {
         </div>
         
         <div className="row">
-            <div className="col">
-            
-            <label for="addressInput" className="form-label">House Address</label>
-            <input className="form-control" type="text" id = "addressInput" name= "addressInput" /*onChange={(e)=>{changeaddress(e.target.value)}}*/  placeholder="House#, City"/>
+            <label for="nameInput" className="form-label">First Name</label>
+            <input className="form-control" type="text" id = "nameInput" name= "nameInput" /*onChange={(e)=>{changename(e.target.value)}}*/ placeholder="firstname lastname"/>
             <br></br>
-            <label for="contactInput" className="form-label">Contact</label>
-            <input className="form-control" id = "contactInput" name="contactInput" /*onChange={(e)=>{changecontact(e.target.value)}}*/  type="text" placeholder="+92 300 *******" />
-            <br></br>
-
-            <br></br>
-            </div>
-            <div className="col" style ={{ margin: "0 50px"}}>
-            <label for="nameInput" className="form-label">Name</label>
+            <label for="nameInput" className="form-label">Last Name</label>
             <input className="form-control" type="text" id = "nameInput" name= "nameInput" /*onChange={(e)=>{changename(e.target.value)}}*/ placeholder="firstname lastname"/>
             <br></br>
             <label for="passwordInput" className="form-label">Password</label>
             <input className="form-control" type="password" id = "passwordInput" name= "passwordInput" /*onChange={(e)=>{changepassword(e.target.value)}}*/ placeholder="********"/>
             <br></br>
+            
+            <label for="contactInput" className="form-label">Contact</label>
+            <input className="form-control" id = "contactInput" name="contactInput" /*onChange={(e)=>{changecontact(e.target.value)}}*/  type="text" placeholder="+92 300 *******" />
+            <br></br>
+            
+            <label for="addressInput" className="form-label">House Address</label>
+            <input className="form-control" type="text" id = "addressInput" name= "addressInput" /*onChange={(e)=>{changeaddress(e.target.value)}}*/  placeholder="House#, City"/>
+            <br></br>
+            
+            <br></br>
+            
+            <label for="addressInput" className="form-label">City</label>
+            <input className="form-control" type="text" id = "addressInput" name= "addressInput" /*onChange={(e)=>{changeaddress(e.target.value)}}*/  placeholder="House#, City"/>
+            <br></br>
+            <label for="contactInput" className="form-label">State</label>
+            <input className="form-control" id = "contactInput" name="contactInput" /*onChange={(e)=>{changecontact(e.target.value)}}*/  type="text" placeholder="+92 300 *******" />
+            <br></br>
+
+            <br></br>
+            
+            <label for="addressInput" className="form-label">Country</label>
+            <input className="form-control" type="text" id = "addressInput" name= "addressInput" /*onChange={(e)=>{changeaddress(e.target.value)}}*/  placeholder="House#, City"/>
+            <br></br>
+
             <button type="button" /*onClick={done}*/ className="btn btn-success">Apply</button>
             <br></br>
-            </div>
-            <div className="col-4">
+            
+            <br></br>
             <button style = {{margin: '24px 0px 0px 0px'}}type="button" className="btn btn-danger" /*onClick={()=>{deleteaccount()}}*/>Delete My Account</button>
-            </div>
         </div>
         </div>   
     )
