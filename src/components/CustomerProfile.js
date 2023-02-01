2131174d3c19e9b4498056ee893979ff2f23eb64
tarar7 <23100225@lums.edu.pk>
2023-02-01 13:54:20 +0500

    Update CustomerProfile.js

--- src/components/CustomerProfile.js
+++ src/components/CustomerProfile.js
@@ -3,37 +3,31 @@ import React from 'react'
 import {useNavigate} from "react-router-dom";
 import {reactLocalStorage} from 'reactjs-localstorage';
 import axios from 'axios';
+import { useEffect } from 'react';
 
 function CustomerProfile() {
     let userID = reactLocalStorage.get('userID', "", true);
-    const [cust, setCust] = React.useState({
-        firstName: '',
-        lastName: '',
-        emailAddress: '',
-        password: 'password',
-        phoneNumber: '',
-        address: '',
-        city: '',
-        country: ''
-    })
+    const [cust, setCust] = React.useState({})
+    let navigate = useNavigate();
 
-    axios.post('https://pacific-sands-58031.herokuapp.com/user/viewprofile', {
-        userID: userID
-    })
-    .then(function(response) {
-        if (response.data.message === 'success'){
-            let d = response.data.data;
-            let x = cust;
-            x.firstName = d.firstName
-            x.lastName = d.lastName
-            x.emailAddress = d.emailAddress
-            x.phoneNumber = d.phoneNumber
-            x.address = d.address
-            x.city = d.city
-            x.country = d.country
-            setCust(x)
-        }
-    })
+    useEffect(() => {
+        axios.post('https://pacific-sands-58031.herokuapp.com/user/viewprofile', {
+            userID: userID
+        })
+        .then(function(response) {
+            if (response.data.message === 'success'){
+                let x = cust
+                x = response.data.data
+                x.password = '********'
+                setCust(x)
+            }
+            else{
+                alert(response.data.message)
+            }
+        }, cust)
+    }, []);
+    
+    console.log(cust)
 
     function changeFirstName(e){
         let x = cust;
@@ -52,7 +46,7 @@ function CustomerProfile() {
     }
     function changePassword(e){
         let x = cust;
-        x.address = e;
+        x.password = e;
         setCust(x)
     }
     function changePhoneNumber(e){
@@ -70,25 +64,22 @@ function CustomerProfile() {
         x.city = e;
         setCust(x)
     }
-    function changeState(e){
-        let x = cust;
-        x.state = e;
-        setCust(x)
-    }
     function changeCountry(e){
         let x = cust;
         x.country = e;
         setCust(x)
     }
     function done(){
-        axios.post('http://localhost:8000/update_customer_info',cust)
+        axios.post('https://pacific-sands-58031.herokuapp.com/user/editprofile', cust)
             .then(function(res) {
                 console.log(res)
                 alert(res);                        
             })
             .catch(function(err) {
                 console.log(err);
-        })}
+        })
+        navigate('/')
+    }
 
     return (
         <div className="container" style={{ margin: "50px 150px", height:"50%", backgroundColor:"white", position:"relative", width:"100%"}}>
@@ -105,39 +96,36 @@ function CustomerProfile() {
         
         <div className="row">
             <label for="nameInput" className="form-label">First Name</label>
-            <input className="form-control" type="text" onChange={(e)=>{changeFirstName(e.target.value)}} placeholder="firstname"/>
+            <input className="form-control" type="text" onChange={(e)=>{changeFirstName(e.target.value)}} placeholder={cust.firstName}/>
             <br></br>
             <label for="nameInput" className="form-label">Last Name</label>
-            <input className="form-control" type="text" onChange={(e)=>{changeLastName(e.target.value)}} placeholder="Lastname"/>
+            <input className="form-control" type="text" onChange={(e)=>{changeLastName(e.target.value)}} placeholder={cust.lastName}/>
             <br></br>
             <label for="nameInput" className="form-label">Email</label>
-            <input className="form-control" type="text" onChange={(e)=>{changeEmail(e.target.value)}} placeholder="Email"/>
+            <input className="form-control" type="text" onChange={(e)=>{changeEmail(e.target.value)}} placeholder={cust.emailAddress}/>
             <br></br>
             <label for="passwordInput" className="form-label">Password</label>
-            <input className="form-control" type="password" onChange={(e)=>{changePassword(e.target.value)}} placeholder="********"/>
+            <input className="form-control" type="password" onChange={(e)=>{changePassword(e.target.value)}} placeholder={cust.password}/>
             <br></br>
             
             <label for="contactInput" className="form-label">Contact</label>
-            <input className="form-control" type="text" onChange={(e)=>{changePhoneNumber(e.target.value)}} placeholder="+92 300 *******" />
+            <input className="form-control" type="text" onChange={(e)=>{changePhoneNumber(e.target.value)}} placeholder={cust.phoneNumber} />
             <br></br>
             
             <label for="addressInput" className="form-label">House Address</label>
-            <input className="form-control" type="text" onChange={(e)=>{changeAddress(e.target.value)}}  placeholder="House Address"/>
+            <input className="form-control" type="text" onChange={(e)=>{changeAddress(e.target.value)}}  placeholder={cust.address}/>
             <br></br>
             
             <br></br>
             
             <label for="addressInput" className="form-label">City</label>
-            <input className="form-control" type="text" onChange={(e)=>{changeCity(e.target.value)}}  placeholder="City"/>
-            <br></br>
-            <label for="contactInput" className="form-label">State</label>
-            <input className="form-control" onChange={(e)=>{changeState(e.target.value)}}  type="text" placeholder="State" />
+            <input className="form-control" type="text" onChange={(e)=>{changeCity(e.target.value)}}  placeholder={cust.city}/>
             <br></br>
 
             <br></br>
             
             <label for="addressInput" className="form-label">Country</label>
-            <input className="form-control" type="text" onChange={(e)=>{changeCountry(e.target.value)}} placeholder="Country"/>
+            <input className="form-control" type="text" onChange={(e)=>{changeCountry(e.target.value)}} placeholder={cust.country}/>
             <br></br>
             <div>
             <br/>
