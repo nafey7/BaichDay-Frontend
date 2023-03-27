aa7e4b2923a730410f5591ff68321b3937eb4d53
Nashit50 <nashitiftikhar50@gmail.com>
2023-03-27 22:51:17 +0500

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,9 +5,9 @@ import Toolbar from '@mui/material/Toolbar';
 import IconButton from '@mui/material/IconButton';
 import SearchIcon from '@mui/icons-material/Search';
 import AccountCircle from '@mui/icons-material/AccountCircle';
-import {Link} from 'react-router-dom'
-import {reactLocalStorage} from 'reactjs-localstorage';
-import {useNavigate} from "react-router-dom";
+import { Link } from 'react-router-dom'
+import { reactLocalStorage } from 'reactjs-localstorage';
+import { useNavigate } from "react-router-dom";
 import axios from 'axios'
 import { useFormik } from 'formik';
 import Input from '@mui/material/Input';
@@ -21,12 +21,12 @@ import { ReactComponent as Logo } from './logo.svg';
 export default function Navbar() {
   const [Check, setCheck] = React.useState(false);
   let navigate = useNavigate();
-  function logout(){
+  function logout() {
     let userID = reactLocalStorage.get('userID',);
     axios.post('https://pacific-sands-58031.herokuapp.com/user/viewprofile', {
-        userID: userID
+      userID: userID
     })
-    .then(function (response) {
+      .then(function (response) {
         if (response.data.message === 'success') {
           reactLocalStorage.remove('token');
           reactLocalStorage.remove('userID');
@@ -34,31 +34,31 @@ export default function Navbar() {
           window.location.reload(true)
         }
         else {
-            alert(response.data.message)
+          alert(response.data.message)
         }
-    })
+      })
   }
-  function viewprofile(){
+  function viewprofile() {
     navigate('/CustomerProfile')
   }
-  function history(){
+  function history() {
     navigate('/History')
   }
-  function wallet(){
+  function wallet() {
     navigate('/wallet')
   }
-  function chat(){
+  function chat() {
     navigate('/chatlist')
   }
   let username
   React.useEffect(() => {
     username = reactLocalStorage.get('userID', "", true);
-    if(username !== ""){
+    if (username !== "") {
       setCheck(true);
     }
 
   }, username)
-  
+
   const formik = useFormik({
     initialValues: {
       name: "",
@@ -70,37 +70,37 @@ export default function Navbar() {
         name: values.name
       }
       )
-      .then(function (response) {
-        console.log(response)
-        if(response.data.message === "success")
-        {
-          if(response.data.length===0){
-            alert("Item not Found")
+        .then(function (response) {
+          console.log(response)
+          if (response.data.message === "success") {
+            if (response.data.length === 0) {
+              alert("Item not Found")
+            }
+            else {
+              console.log(response.data)
+              navigate('/product', {
+                state: {
+                  _id: response.data.data[0]._id,
+                  description: response.data.data[0].description,
+                  name: response.data.data[0].name,
+                  image: response.data.data[0].image,
+                  bid: response.data.data[0].bid,
+                }
+              })
+            }
           }
-          else{
-            console.log(response.data)
-            navigate('/product',{ state: {
-              _id: response.data.data[0]._id,
-              description: response.data.data[0].description,
-              name: response.data.data[0].name,
-              image: response.data.data[0].image,
-              bid: response.data.data[0].bid,
-            }})
+          else {
+            alert("Object Not Found");
           }
-        }
-        else
-        {
-          alert("Object Not Found");
-        }
-        
-      })
-      .catch(function (error) {
-        console.log(error);
-        alert("Error")
-      });
+
+        })
+        .catch(function (error) {
+          console.log(error);
+          alert("Error")
+        });
     },
   });
-  
+
   const [anchorEl1, setAnchorEl1] = React.useState(null);
   const [anchorEl2, setAnchorEl2] = React.useState(null);
   const [anchorEl3, setAnchorEl3] = React.useState(null);
@@ -124,11 +124,11 @@ export default function Navbar() {
   };
 
   return (
-    <Box sx={{ flexGrow: 1}}>
-      <AppBar position="fixed" sx={{backgroundColor:"black"}}>
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar position="fixed" sx={{ backgroundColor: "black" }}>
         <Toolbar>
-          <Link to={{pathname: "/"}} style={{margin:"0 15px",color: "#a7ac38"}}><Logo style={{width:"200px", height:"70px"}} className="d-inline-block"/></Link>
-          <button className="btn" aria-controls="menu1" aria-haspopup="true" onClick={handleClick1} style={{fontSize:"150%", marginLeft:"50px", color:"white"}}>Auctions</button>
+          <Link to={{ pathname: "/" }} style={{ margin: "0 15px", color: "#a7ac38" }}><Logo style={{ width: "200px", height: "70px" }} className="d-inline-block" /></Link>
+          <button className="btn" aria-controls="menu1" aria-haspopup="true" onClick={handleClick1} style={{ fontSize: "150%", marginLeft: "50px", color: "white" }}>Auctions</button>
           <Menu
             id="menu1"
             anchorEl={anchorEl1}
@@ -141,8 +141,8 @@ export default function Navbar() {
           </Menu>
           <button className="btn" id="demo-positioned-button2"
             aria-controls="menu2" aria-haspopup="true" onClick={handleClick2}
-            style={{fontSize:"150%", marginLeft:"20px", color:"white"}}>
-              Categories
+            style={{ fontSize: "150%", marginLeft: "20px", color: "white" }}>
+            Categories
           </button>
           <Menu
             id="menu2"
@@ -160,38 +160,46 @@ export default function Navbar() {
             <MenuItem onClick={handleClose}>Cars</MenuItem>
             <MenuItem onClick={handleClose}>Others</MenuItem>
           </Menu>
-          <button className="btn btn-success" onClick={() =>{navigate("/AddProduct")}} style={{fontSize:"150%", marginLeft:"20px", borderRadius:"15px"}}>Sell With Us</button>
-          <FormControl sx={{backgroundColor: 'white', marginLeft:"40%", borderRadius:"15px"}} variant="standard">
-          <Input
-            type={'text'}
-            id="name"
-            disableUnderline = "false"
-            placeholder="   Search Product"
-            style={{paddingLeft: "10px"}}
-            onChange={formik.handleChange}
-            endAdornment={
+          <button className="btn btn-success" onClick={() => { navigate("/AddProduct") }} style={{ fontSize: "150%", marginLeft: "20px", borderRadius: "15px" }}>Sell With Us</button>
+          <FormControl sx={{ backgroundColor: 'white', marginLeft: "40%", borderRadius: "15px" }} variant="standard">
+            <Input
+              sx={{
+                height: 'auto',
+                width: '300px',
+                fontSize: '15px',
+              }}
+              type={'text'}
+              id="name"
+              disableUnderline="false"
+              placeholder="   Search Product"
+              style={{ paddingLeft: "10px" }}
+              onChange={formik.handleChange}
+              endAdornment={
                 <IconButton
                   onClick={formik.handleSubmit}
                 >
-                <SearchIcon/>
+                  <SearchIcon />
                 </IconButton>
-            }
-            label="search"
-          />
-        </FormControl>
-        {Check?(
-        <IconButton
-            size="large"
-            aria-label="account of current user"
-            aria-controls="menu3" 
-            aria-haspopup="true" 
-            onClick={handleClick3}
-            color="inherit"
-          >
-            <AccountCircle sx={{fontSize: "200%"}} />
-          </IconButton>
-          ):(
-            <button className="btn btn-dark" onClick={() =>{navigate("/login")}} style={{fontSize:"100%", color:"white", borderRadius:"10px" ,margin:"0 0px 0 20px"}}>Sign In</button>)}
+              }
+              label="search"
+            />
+          </FormControl>
+          {Check ? (
+            <IconButton
+              sx={{
+                fontSize: '20px',
+              }}
+              size="large"
+              aria-label="account of current user"
+              aria-controls="menu3"
+              aria-haspopup="true"
+              onClick={handleClick3}
+              color="inherit"
+            >
+              <AccountCircle sx={{ fontSize: "200%" }} />
+            </IconButton>
+          ) : (
+            <button className="btn btn-dark" onClick={() => { navigate("/login") }} style={{ fontSize: "100%", color: "white", borderRadius: "10px", margin: "0 0px 0 20px" }}>Sign In</button>)}
           <Menu
             id="menu3"
             anchorEl={anchorEl3}
@@ -218,6 +226,6 @@ export default function Navbar() {
       <br></br>
       <br></br>
     </Box>
-    
+
   );
 }
