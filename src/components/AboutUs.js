310434ffea24af6b301ed73ef42a0b20aedc4054
Nashit50 <nashitiftikhar50@gmail.com>
2023-03-27 16:32:14 +0500

    About Us Finalized

--- src/components/AboutUs.js
+++ src/components/AboutUs.js
@@ -10,15 +10,15 @@ function AboutUs() {
           <Row >
             <Col >
               <div className="about-us-text"  >
-                <h1 className="about-us-title">About Us</h1>
+                <h1 className="about-us-title" style={{float: "left"}}>About Us</h1>
                 <br></br>
                 <div className="about-us-image">
-                  <img src="logo.png" alt="about us" />
+                  <img src="logo.png" alt="about us" style={{width: "100%", height: "450px", marginTop: "10%", borderRadius: "8px", boxShadow: "5px 5px 15px"}}/>
                 </div>
                 <br></br>
                 <br></br>
-                <div style={{width: "75%", paddingLeft:"25%"}}>
-                <p className="about-us-description" style={{textAlign: "justify"}}>Welcome to Baichday, the ultimate online bidding platform that connects buyers and sellers from all around the world. Our mission is to provide a safe and reliable platform for users to buy and sell goods and services through a transparent bidding process. At Baichday, we understand the importance of creating a trustworthy and user-friendly platform for online bidding. That's why we have designed our website to be intuitive and easy to navigate, making it simple for anyone to use.</p>
+                <div style={{width: "50%"}}>
+                <p className="about-us-description" style={{textAlign: "justify", float: "left"}}>At Baichday, we are passionate about providing a platform that enables buyers and sellers from all around the world to connect and conduct business in a safe and secure manner. Our team is dedicated to creating an online marketplace that is user-friendly, intuitive, and transparent. We believe that the key to success in the online bidding industry is trust, and that's why we have made it our top priority to establish a platform that fosters transparency and openness. We want our users to feel confident and secure when using our platform, which is why we have implemented a number of measures to protect both buyers and sellers. Our platform is designed to be easy to use, with a streamlined bidding process that ensures a smooth and hassle-free experience for all users. We have also implemented various features to enhance the overall user experience, such as a messaging system that allows buyers and sellers to communicate directly with each other. At Baichday, we believe that the world is a better place when people can connect and do business with each other, regardless of geographic location or cultural background. We are committed to fostering a global community of buyers and sellers who can rely on our platform to conduct transactions with confidence and ease.</p>
                 </div>
               </div>
             </Col>
@@ -30,12 +30,13 @@ function AboutUs() {
       <br></br>
       <div className="features-section">
         <Container>
-          <h2 className="section-title">Our Features</h2>
+          <h2 className="section-title" style={{fontSize: "25px"}}>Our Promises</h2>
+          <br></br>
           <Row>
             <Col md={4}>
               <div className="feature-box">
                 <div className="feature-icon">
-                  <i className="fas fa-gift"></i>
+                  <i className="fas fa-gift" style={{height: "0px", width: "0px"}}></i>
                 </div>
                 <div className="feature-content">
                   <h4>Quality Products</h4>
@@ -61,7 +62,7 @@ function AboutUs() {
                 </div>
                 <div className="feature-content">
                   <h4>Customer Support</h4>
-                  <p>We have a dedicated team of customer support representatives that are always available to assist you with any issues you may face. We are committed to providing you with exceptional support.</p>
+                  <p>We are committed to delivering top-notch customer support, with a focus on personalized assistance and timely resolution of issues.</p>
                 </div>
               </div>
             </Col>
