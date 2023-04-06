b607fc168f658a30526240449c07958fca44b126
tarar7 <23100225@lums.edu.pk>
2023-04-06 15:32:38 +0500

    Timer loading fixed

--- src/components/UserHome.js
+++ src/components/UserHome.js
@@ -8,25 +8,28 @@ import { useNavigate } from "react-router-dom";
 
 function UserHome(props) {
     const [dataprops, setDataprops] = React.useState([{ name: "", image: "img.png", bid: [0] }]);
+    const [dataCheck, setDataCheck] = React.useState(false);
     let navigate = useNavigate();
     let rand = Math.floor(Math.random() * 1000);
     React.useEffect(()=> {
         if(props.title ==="Featured Products"){
           axios.get('https://pacific-sands-58031.herokuapp.com/product/')
           .then(function(res) {
-                console.log("in featured products")
-              console.log(res.data.data)
-              setDataprops(res.data.data)          
+              console.log("in featured products");
+              console.log(res.data.data);
+              setDataprops(res.data.data);
+              setDataCheck(true);          
           }, dataprops)
           .catch(function(err) {
               console.log(err);
         })
         }else{
-          console.log("in categories")
+          console.log("in categories");
           axios.post('https://pacific-sands-58031.herokuapp.com/product/category', {category: props.title})
           .then(function(res) {
             console.log(res.data.data)
-              setDataprops(res.data.data)          
+              setDataprops(res.data.data);
+              setDataCheck(true);          
           }, dataprops)
           .catch(function(err) {
               console.log(err);
@@ -44,11 +47,14 @@ function UserHome(props) {
 
             <Box sx={{ display: 'flex', overflowY: 'auto' }}>
                 
-                    
-                <DisplayCards test={dataprops}/>
+                {dataCheck && (
+                    <DisplayCards test={dataprops}/>
+
+                )} 
                 <div style={{backgroundSize: 'cover', width: '17%', height: '70%', position: 'fixed', top: '18%', right: '0px', }}>
                     <img style={{maxWidth: '100%', maxHeight: '100%', paddingRight: '5%'}} src="221.jpg" alt="advert" />
-                </div>
+                </div> 
+                
             </Box>
             <br />
             <br />
