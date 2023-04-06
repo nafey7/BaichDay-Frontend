933a3e5fcce43370cf1aa5e8aa550f231f582ed3
nafey7 <nafeymoiz07@gmail.com>
2023-04-06 14:09:39 +0500

    Admin updated for graphs

--- src/components/AdminHome.js
+++ src/components/AdminHome.js
@@ -32,17 +32,30 @@ export default function AdminHome() {
   const [income, setIncome] = React.useState(0);
   const [products, setProducts] = React.useState(0);
   const [active_users, setActive] = React.useState(0);
+  const [alllabels, setAllLabels] = React.useState([]);
+  const [revenueCategory, setrevenueCategory] = React.useState([]);
+  const [itemsCategory, setitemsCategory] = React.useState([]);
   const userID = reactLocalStorage.get('adminID');
 
   React.useEffect(()=> {
 
     axios.post('https://pacific-sands-58031.herokuapp.com/admin/home', {adminID: userID, duration: "all time"}) //duration: all time, 7 days, 30 days, 3 months
     .then(function(res) {
-        console.log(res)
+
+      console.log(res)
         setUsers(res.data.data.sales.NumberOfUsers)
         setIncome(res.data.data.sales.income)
         setProducts(res.data.data.sales.productsSold)
         setActive(res.data.data.sales.ActiveUsers)
+        setitemsCategory(res.data.data.itemsByCategory)
+        setrevenueCategory(res.data.data.revenueByCategory)
+        setAllLabels(res.data.data.labels)
+
+        console.log(itemsCategory);
+        console.log(revenueCategory);
+        console.log(alllabels)
+
+        
         // setChartData({
         //   ...chartData,
         //   series: res.data.data.sales.chart,
